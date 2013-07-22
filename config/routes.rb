5add683c5b92497780b7613ea1b3d281b0a13d83
Mario Andrés Correa <admin@macool.me>
2013-07-22 14:30:20 -0500

    testing.

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Blog::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  root to: "home#index"
 
   # Example resource route with options:
   #   resources :products do
