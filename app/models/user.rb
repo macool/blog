7736c2429e6ec4dacee8f04711213aa65608ceb9
Mario Andrés Correa <admin@macool.me>
2013-07-22 16:30:23 -0500

    added validations of password to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,7 @@ class User < ActiveRecord::Base
 # validations:
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+  validates_length_of :password, :minimum => 3, :message => "password must be at least 3 characters long", :if => :password
+  validates_confirmation_of :password, :message => "should match confirmation", :if => :password
 
 end
