a0060c956f7a25c96c8d2e2653a317acf92ec5aa
Mario Andrés Correa <admin@macool.me>
2013-07-22 16:20:22 -0500

    added relation between User<>Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ActiveRecord::Base
 
+# relations:
+  belongs_to :user
 
+# validations:
   validates :slug, uniqueness: true, presence: true
   validates :title, presence: true
+  validates :user_id, presence: true
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
+# relations:
+  has_many :posts
 
+# validations:
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
 
