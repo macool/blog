3d07751b76ecb104b316d1eaefd027a1cf56b4b0
Mario Andrés Correa <admin@macool.me>
2013-07-23 13:10:14 -0500

    adding comments (not finished yet)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,25 @@
 class CommentsController < ApplicationController
   
   def create
-    
+    person = Guest.update_with_email(params[:comment][:person])
+    @comment = Comment.new comment_params
+    @comment.person = person
+    respond_to do |format|
+      format.js {
+        if @comment.save
+          render 'created'
+        else
+          render 'not_created'
+        end
+      }
+    end
+  end
+
+
+private
+
+  def comment_params
+    params[:comment].permit :post_id, :content
   end
   
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,5 @@
 class Guest < ActiveRecord::Base
+  include ActiveModel::ForbiddenAttributesProtection
 
 # relations:
   has_many :comments
@@ -6,5 +7,13 @@ class Guest < ActiveRecord::Base
 # validations:
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
+
+# class methods:
+  def self.update_with_email( params )
+    attributes = ActionController::Parameters.new(params).permit(:name, :email, :website)
+    guest = Guest.find_or_initialize_by(email: attributes[:email])
+    guest.update_attributes(attributes)
+    guest
+  end
 
 end
