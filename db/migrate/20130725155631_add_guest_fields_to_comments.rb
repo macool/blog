aaec2a1b4aadbb2adc006c17813181040e25035a
Mario Andrés Correa <admin@macool.me>
2013-07-25 11:05:00 -0500

    removed Guest model (now those fields belong to Comment)

---
+++ db/migrate/20130725155631_add_guest_fields_to_comments.rb
@@ -0,0 +1,16 @@
+class AddGuestFieldsToComments < ActiveRecord::Migration
+  def up
+    change_table :comments do |t|
+      t.string :email
+      t.string :name
+      t.string :website
+    end
+    add_index :comments, :email
+  end
+  def down
+    remove_index :comments, :email
+    change_table :comments do |t|
+      t.remove :email, :name, :website
+    end
+  end
+end
