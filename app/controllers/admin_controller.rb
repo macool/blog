ad2ca05481aba357358db117be6b57199bbf7b17
Mario Andrés Correa <admin@macool.me>
2013-07-25 01:01:05 -0500

    I18n

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,7 +7,7 @@ protected
 
   def confirm_user_is_admin
     unless current_user and current_user.is_admin
-      flash["alert-error"] = "you kinda' forgot something, dude."
+      flash["alert-error"] = I18n.t("login_required", default: "you kinda' forgot something, dude.")
       redirect_to login_path
     end
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,13 @@ class SessionsController < ApplicationController
     respond_to do |format|
       if @user = login(params[:username],params[:password])
         format.html {
-          flash["alert-success"] = "Login successful."
+          flash["alert-success"] = I18n.t("login_successful", default: "Login successful.")
           redirect_back_or_to root_path
         }
         format.json { render :json => @user, :status => :created, :location => @user }
       else
         format.html {
-          flash["alert-error"] = "Login failed."
+          flash["alert-error"] = I18n.t("login_failed", default: "Login failed.")
           render :action => :index
         }
         format.json { render :json => @user.errors, :status => :unprocessable_entity }
@@ -23,7 +23,7 @@ class SessionsController < ApplicationController
 
   def destroy
     logout
-    flash["alert-info"] = "Logged out!"
+    flash["alert-info"] = I18n.t("logged_out", default: "Logged out!")
     redirect_back_or_to root_path
   end
 
