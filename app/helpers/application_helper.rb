aeba6b8bdd0659764c9ce45675830175de9e30fb
Mario Andrés Correa <admin@macool.me>
2013-07-25 12:59:27 -0500

    moved logic into helper js_response. Usage: js_response(js_selector, rendered_content);

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,15 @@ module ApplicationHelper
     if params[:controller] == "admin/#{name}" then "class=active" else "" end
   end
   
+  def js_response(selector, rendered_content)
+    js = <<-js
+      (function(){
+        var node = $("<span />", { html: "#{j rendered_content}"} );
+        Helpers.TriggerHelpers.trigger(node);
+        $("#{selector}").html(node.html());
+      }());
+    js
+    raw js
+  end
+  
 end
