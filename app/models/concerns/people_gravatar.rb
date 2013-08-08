c53d2f13d2a0857e0c58e42d2f357ae57201255e
Mario Andrés Correa <admin@macool.me>
2013-08-08 11:37:02 -0500

    caching image_url and moved concern to a proper place

--- app/models/concerns/people_gravatar.rb
+++ app/models/concerns/people_gravatar.rb
@@ -1,7 +1,7 @@
 module PeopleGravatar
   
   def image_url
-    "http://www.gravatar.com/avatar/#{gravatar_hash}"
+    @image_url ||= "http://www.gravatar.com/avatar/#{gravatar_hash}"
   end
   def gravatar_hash
     @gravatar_hash ||= Digest::MD5.hexdigest(email)
