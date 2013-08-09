b58711c52acae983bede6022f0c2690efbb2140a
Mario Andrés Correa <admin@macool.me>
2013-08-09 16:40:50 -0500

    removed unused eager loading

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.published.includes(:comments).find_by!(slug: params[:id])
+    @post = Post.published.find_by!(slug: params[:id])
     @comments = if current_user and current_user.is_admin then @post.comments.order("id ASC") else @post.comments.approved end
     @comment = Comment.new post_id: @post.id
   end
